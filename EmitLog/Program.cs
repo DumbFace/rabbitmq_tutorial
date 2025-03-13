ee06ef0570dbfc1c1b12d4ac47ed4aec809bb716
LenevoKangFarmFun <gv.runfast@gmail.com>
2025-03-13 18:00:24 +0700

    first commit

---
+++ EmitLog/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using newtask;
+using System.Text;
+namespace EmitLog;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        var connect = await factory.CreateConnectionAsync();
+        var channel = await connect.CreateChannelAsync();
+
+        // await channel.QueueDeclareAsync(String.Empty, true, false, false, null);
+        await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+        // var message = new NewTask();
+        var message = GetMessage(args);
+        var body = Encoding.UTF8.GetBytes(message);
+        var properties = new BasicProperties
+        {
+            Persistent = true
+        };
+        channel.BasicReturnAsync += (sender, ea) =>
+        {
+            Console.WriteLine($"Message bị trả về! ReplyCode: {ea.ReplyCode}, ReplyText: {ea.ReplyText}");
+            return Task.CompletedTask;
+        };
+
+        await channel.BasicPublishAsync(exchange: "logs", String.Empty, mandatory: true, basicProperties: properties, body);
+        Console.WriteLine($" [x] Sent {message}");
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+    }
+}
---
+++ ReceiveLogs/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace ReceiveLogs;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        var connection = await factory.CreateConnectionAsync();
+        var channel = await connection.CreateChannelAsync();
+
+        await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+        QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync(durable: true);
+        string queueName = queueDeclareResult.QueueName;
+
+        await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: string.Empty);
+        Console.WriteLine(" [*] Waiting for logs.");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += (model, ea) =>
+        {
+            byte[] body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] {message}");
+
+            return Task.Run(() => { });
+        };
+
+        await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
